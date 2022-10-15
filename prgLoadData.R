7e7a19e27cb5473aa0da8fde63cfda838140a6b6
Pierre Aldama <pierre.aldama@gmail.com>
2022-10-15 12:44:09 +0200

    Update

--- prgLoadData.R
+++ prgLoadData.R
@@ -6,13 +6,6 @@ url<-"https://www.insee.fr/fr/statistiques/fichier/4771989/T79JDEC.csv"
 dest<-"./HistoricalDataMortality.csv"
 data<-download.file(url, dest)
 
-# Insee data on mortality for 2020/2021/2022
-url<-"https://www.insee.fr/fr/statistiques/fichier/4487988/2022-09-30_detail.zip"
-dest<-"./dataMortatility.zip"
-download.file(url, dest)
-zipfile<-paste(CurrentWorkingDir,"/dataMortatility.zip",sep="")
-unzip(zipfile)
-
 # data from SPF
 url<-"https://www.data.gouv.fr/fr/datasets/r/d3a98a30-893f-47f7-96c5-2f4bcaaa0d71"
 dest<-'./SPF.csv'
@@ -27,3 +20,10 @@ Sentinelles<-download.file(url, dest)
 url <- "https://www.data.gouv.fr/fr/datasets/r/08c18e08-6780-452d-9b8c-ae244ad529b3"
 dest <- "./sivic_donneeshospit.csv"
 sivic<- download.file(url,dest)
+
+
+# Insee data on mortality for 2020/2021/2022
+url<-"http://www.insee.fr/fr/statistiques/fichier/4487988/2022-09-30_detail.zip"
+dest<-"./dataMortatility.zip"
+download.file(url, dest)
+unzip("./dataMortatility.zip")
